0b33143e4429fc26deb520cbc135fc74c0516515
Shashank Yadav <shashank1558@gmail.com>
2020-07-11 10:58:00 +0530

    Package setup for smooth imports

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 """
 This solves the problem of relative imports.
-Use command $ pip install -e .
+To install use following command in setup.py directory 
+    $ pip install -e .
 Direct imports from root directory will be enabled
+
 Courtesy : https://stackoverflow.com/a/50194143
+
+Note: There is a chance of interference from poetry (package manager used in this project)
+ .lock and .toml, temporarily move poetry files somewhere else and install this package 
 """
 
 from setuptools import setup, find_packages
