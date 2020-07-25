a5f149fc7601411374d7b58ceb8f2b5b829d61d3
Shashank Yadav <shashank1558@gmail.com>
2020-07-25 09:07:19 +0530

    Reinilializing for large files

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ Direct imports from root directory will be enabled
 
 Courtesy : https://stackoverflow.com/a/50194143
 
-Note: There is a chance of interference from poetry (package manager used in this project)
- .lock and .toml, temporarily move poetry files somewhere else and install this package 
 """
 
 from setuptools import setup, find_packages
